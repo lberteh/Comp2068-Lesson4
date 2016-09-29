8c9076cf132040ba54f8c5b58d1229091eedd24e
Lucas Schoenardie <lberteh@gmail.com>
2016-09-29 13:46:28 -0400

    added more users

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   // res.send('respond with a resource');
-  var users = ['Mackenzie', 'Lucas', 'Sarah'];
+  var users = ['Mackenzie', 'Lucas', 'Adam', 'Amanda', 'George'];
   res.render('users',
     { title: 'User List',
       users: users
